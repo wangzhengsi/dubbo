6b48f5e30e238147e822cb1e3bbef0aef8e47745
wangzhengsi <17614427501@163.com>
2023-12-23 17:50:47 +0800

    源码阅读

---
+++ dubbo-demo/dubbo-demo-spring-boot-wzs/dubbo-demo-spring-boot-provider-wzs/src/main/java/org/apache/dubbo/springboot/demo/provider/TestServiceConfig.java
@@ -0,0 +1,33 @@
+package org.apache.dubbo.springboot.demo.provider;
+
+import org.apache.dubbo.common.constants.CommonConstants;
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.ProtocolConfig;
+import org.apache.dubbo.config.RegistryConfig;
+import org.apache.dubbo.config.ServiceConfig;
+import org.apache.dubbo.config.bootstrap.DubboBootstrap;
+import org.apache.dubbo.springboot.demo.TempuraService;
+import org.apache.dubbo.springboot.demo.provider.service.TempuraServiceImpl;
+
+/**
+ * @author wangzhengsi
+ * @since 2023/12/23 17:26
+ */
+public class TestServiceConfig {
+    public static void main(String[] args) {
+        startWithBootstrap();
+    }
+
+    private static void startWithBootstrap() {
+        ServiceConfig<TempuraServiceImpl> service = new ServiceConfig<>();
+        service.setInterface(TempuraService.class);
+        service.setRef(new TempuraServiceImpl());
+        DubboBootstrap bootstrap = DubboBootstrap.getInstance();
+        bootstrap.application(new ApplicationConfig("my-provider"))
+            .registry(new RegistryConfig("nacos://127.0.0.1:8848"))
+            .protocol(new ProtocolConfig(CommonConstants.DUBBO, 20881))
+            .service(service)
+            .start()
+            .await();
+    }
+}
